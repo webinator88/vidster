72ecc0021e92c368f4667e73045b9b9e32650bfc
Daler <dmadaminov@allmax.team>
2018-09-02 19:02:51 +0700

    repo initialised

---
+++ index.js
@@ -0,0 +1,39 @@
+import express from 'express';
+
+import data from './data/data.json';
+
+const app = express();
+const PORT = process.env.port || 3000;
+
+// this is public folder for default route "/"
+// note that leading slash isn't permitted 
+app.use(express.static('public'));
+
+// this is public folder for specified route images
+// note that leading slash in first parameter is required
+app.use('/images', express.static('images'));
+
+app.use(express.json());
+
+app.get('/', (req, res) =>
+  res.json(data)
+);
+
+app.get('/item/:id', (req, res) => {
+  const { id } = req.params;
+  const item = data[Number(id)];
+  res.send(item);
+});
+
+app.route('/item')
+  .get((req, res) => res.send(`a get request /item on port ${PORT}\n`))
+  .post((req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+  })
+  .put((req, res) => res.send(`a put request /item on port ${PORT}\n`))
+  .delete((req, res) => res.send(`a delete request /item on port ${PORT}\n`));
+
+app.listen(PORT, () => {
+  console.log(`Your server is running on ${PORT}\n`);
+});
